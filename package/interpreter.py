5f8da59faae9a1229983a1f74a3a76d9185a2ee0
jasper <jasper@vanderstoop.nl>
2017-03-24 18:31:04 +0100

    Packaged the project and added a wrapper to call the matcher.

--- package/interpreter.py
+++ package/interpreter.py
@@ -1,11 +1,12 @@
-import json
-import sys
-import operator
 import functools
-import itertools
+import json
+import operator
+import os
 from collections import defaultdict
+
 from nltk.corpus import stopwords
-from lbl_uri_indexer import knowledge
+
+from .lbl_uri_indexer import knowledge
 
 #text = "is it correct to say that alan turing and noam chomsky are the giants"
 #text = sys.argv[1:]
@@ -22,7 +23,10 @@ def clean_input(text):
 
 def uri_term_mapper(sent_wo_stpwrds):
     trm_uri = {}
-    lbl_uri_index = json.load(open("dbp/lbl-uri_index.json"))
+
+    this_dir, this_filename = os.path.split(__file__)
+    json_file = os.path.join(this_dir, 'dbp/lbl-uri_index.json')
+    lbl_uri_index = json.load(open(json_file))
     for term in sent_wo_stpwrds:
         uri_list_dup = []
         for enti_term, uri in lbl_uri_index.items():
@@ -78,7 +82,9 @@ def calculate_llh(ment_cnt_dbp, list_freq_corpus):
 
 
 def llh_mapping(candidates):
-    unigram_file = open('lexstat/unigrams.json')
+    this_dir, this_filename = os.path.split(__file__)
+    json_file = os.path.join(this_dir, 'lexstat/unigrams.json')
+    unigram_file = open(json_file)
     unigram_dict = json.load(unigram_file)
     concept = {}
     for uri, multiword in candidates.items():
---
+++ package/wrapper.py
@@ -0,0 +1,39 @@
+from . import interpreter as ip
+
+def match_text(input_text):
+    if isinstance(input_text, str):
+        input_text = input_text.split()
+
+    #Clean the input string from stopwords by using the nltk language resource
+    sent_wo_stpwrds = ip.clean_input(input_text)
+    #print("sentences without stopwords")
+    #print(sent_wo_stpwrds)
+
+    print("Map the singlewords to the DBPedia entries and the concerned URI's....")
+    #Map the extracted terms from input string to DBPedia links, and reverse them.
+    uri_term_dict_shallow, lbl_uri_index  = ip.uri_term_mapper(sent_wo_stpwrds)
+    #for x,y in uri_term_dict_shallow.items():
+    #    print(x,y)
+
+    print("Map the URI's of the DBPedia entries to potential multiword...")
+    #Map the uri's to the potential multiwords tha refer to a single concept each
+    uri_multiword = ip.extract_relevant_terms(uri_term_dict_shallow, lbl_uri_index)
+    #for x,y in uri_multiword.items():
+    #    print(x,y)
+
+    print("Print selected candidates...")
+    #Select the most likely multiwords of candidates to be nominated for LLH-based decision
+    candidates = ip.select_candidates(uri_multiword)
+    #for x,y in candidates.items():
+    #    print(x,y)
+
+    print("Nominate candidates for LLH...")
+    #Create the dictionary of the nominated candidates for LLH-bases selection
+    decision_index = ip.llh_mapping(candidates)
+    #for x,y in decision_index.items():
+    #    print(x,y)
+
+    print("And the winner is.......")
+    #Select the most likely entity!
+    the_winner = ip.select_winning_entity(decision_index)
+    return the_winner

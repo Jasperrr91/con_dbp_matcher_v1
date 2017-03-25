788f196a39dedffc8323a91f70d333c6f682544a
jasper <jasper@vanderstoop.nl>
2017-03-25 15:21:11 +0100

    Added a verbose option to either display/hide the status text. Also included a function that transforms everything to lower case.

--- match-with-dbp.py
+++ match-with-dbp.py
@@ -5,7 +5,7 @@ from package.wrapper import match_text
 #text = "is it correct to say that alan turing and noam chomsky are the giants in theoretical computer science"
 input_text = sys.argv[1:]
 
-the_winner = match_text(input_text)
+the_winner = match_text(input_text, verbose=True)
 
 for x,y in the_winner.items():
     print(x,y)
